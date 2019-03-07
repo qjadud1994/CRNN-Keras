d28a3137e0f2a2fef496c342b4186ae358a9ce58
Beom <qjadud1994@gmail.com>
2019-03-07 19:38:06 +0900

    Update Model.py

--- Model.py
+++ Model.py
@@ -61,11 +61,16 @@ def get_Model(training):
     # RNN layer
     lstm_1 = LSTM(256, return_sequences=True, kernel_initializer='he_normal', name='lstm1')(inner)  # (None, 32, 512)
     lstm_1b = LSTM(256, return_sequences=True, go_backwards=True, kernel_initializer='he_normal', name='lstm1_b')(inner)
-    lstm1_merged = add([lstm_1, lstm_1b])  # (None, 32, 512)
+    reversed_lstm_1b = Lambda(lambda inputTensor: K.reverse(inputTensor, axes=1)) (lstm_1b)
+
+    lstm1_merged = add([lstm_1, reversed_lstm_1b])  # (None, 32, 512)
     lstm1_merged = BatchNormalization()(lstm1_merged)
+    
     lstm_2 = LSTM(256, return_sequences=True, kernel_initializer='he_normal', name='lstm2')(lstm1_merged)
     lstm_2b = LSTM(256, return_sequences=True, go_backwards=True, kernel_initializer='he_normal', name='lstm2_b')(lstm1_merged)
-    lstm2_merged = concatenate([lstm_2, lstm_2b])  # (None, 32, 1024)
+    reversed_lstm_2b= Lambda(lambda inputTensor: K.reverse(inputTensor, axes=1)) (lstm_2b)
+
+    lstm2_merged = concatenate([lstm_2, reversed_lstm_2b])  # (None, 32, 1024)
     lstm_merged = BatchNormalization()(lstm2_merged)
 
     # transforms RNN output to character activations:
